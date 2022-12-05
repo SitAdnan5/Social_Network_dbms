a6d80d3aee9e9114d4fd8ef90e2c28e4b5cba999
Nandeesh <nandeeshm2020@gmail.com>
2022-12-05 21:42:00 +0530

    added

---
+++ index.php
@@ -0,0 +1,21 @@
+    <style>
+        <?php 
+        include 'css/style.css';
+        ?>
+    </style>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Solway&display=swap');
+    </style>
+</head>
+<body>
+   <!-- ****************************************nav bar ***************************************** -->
+
+    <nav class="nav-bar">
+        <div class="logo">
+            <h2 class="logo-img">Piper network</h2>
+        </div>
+    </nav>
+    <br>
+   <!-- ****************************************end nav-bar ************************************* -->
+</body>
---
+++ post.php
@@ -0,0 +1,72 @@
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Solway&display=swap');
+    </style>
+</head>
+<body>
+    <!-- *************************************************post-data*********************************************** -->
+   <div class="outer-box">
+
+    <div class="card">
+        <div class="heading">
+            <div class="profile-image"></div>
+            <p class="name">Nandeesh M</p>
+            <p class="time">12:06AM</p>
+        </div>
+        <div class="image">
+            <img src="images/behrouz-sasani-khMxnuosSV4-unsplash.jpg" width="100%">
+        </div>
+        <div class="description">
+            <h2 class="title">
+                Science
+            </h2>
+            <br>
+            <p class="about">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut minus aperiam ducimus quaerat qui voluptatum quod! Dolorum dolor voluptatem similique doloremque magni? Mollitia vitae inventore assumenda dolore ipsum quos odio!
+                Ducimus, laborum ab odio, molestias doloribus facere provident id perspiciatis at amet ut sunt, sequi esse placeat necessitatibus saepe nam qui totam quisquam magni accusantium sed consequatur. Debitis, iste amet?
+                Enim reiciendis fugiat sapiente ad rerum. Asperiores ut molestiae numquam unde aspernatur voluptatibus sapiente cumque quas dolore? Reprehenderit eveniet totam assumenda debitis accusantium voluptate minus autem, corrupti beatae ea dolor?
+            </p>
+        </div>
+        <br>
+        <div class="date">
+            <h4>23-06-2020</h4>
+        </div>
+        <br>
+    </div>
+
+
+    <!--************ Checking for the other post *************-->
+
+    <div class="card">
+        <div class="heading">
+            <div class="profile-image"></div>
+            <p class="name">Nandeesh M</p>
+            <p class="time">12:06AM</p>
+        </div>
+        <div class="image">
+            <img src="images/omid-armin-6G2G6_rq-B0-unsplash.jpg" width="100%">
+        </div>
+        <div class="description">
+            <h2 class="title">
+                Science
+            </h2>
+            <br>
+            <p class="about">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut minus aperiam ducimus quaerat qui voluptatum quod! Dolorum dolor voluptatem similique doloremque magni? Mollitia vitae inventore assumenda dolore ipsum quos odio!
+                Ducimus, laborum ab odio, molestias doloribus facere provident id perspiciatis at amet ut sunt, sequi esse placeat necessitatibus saepe nam qui totam quisquam magni accusantium sed consequatur. Debitis, iste amet?
+            </p>
+        </div>
+        <br>
+        <div class="date">
+            <h4>23-07-2020</h4>
+        </div>
+        <div class="likes">
+            
+        </div>
+        <br>
+    </div>
+
+    <!-- ***********check end********************************* -->
+
+   </div>
+    <!-- *************************************************end***************************************************** -->
+</body>
